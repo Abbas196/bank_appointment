4aca3a06477dcaede651cde2ecb1cd29b1e75fd4
Abbas196 <abbasraza196@gmail.com>
2022-10-20 15:55:46 -0500

    Initial commit

---
+++ src/test/java/com/bank_appointment/app/bank_appointment/BankAppointmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bank_appointment.app.bank_appointment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankAppointmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
