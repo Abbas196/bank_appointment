2306906d5b36f954d1793fe2eb6676fbbfd1163f
Abbas196 <abbasraza196@gmail.com>
2022-11-04 03:35:27 -0500

    front end navigation Bar created

--- FrontEnd/book-app-frontend/src/App.js
+++ FrontEnd/book-app-frontend/src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Navbar from './components/Navbar';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages';
+import About from './pages/About';
+import Schedule from './pages/Schedule';
+import Checking from './pages/Checking';
+import logo from './bankLogo.png';
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+return (
+	<Router>
+  <img src={logo} width = '250' height = '120' alt = "Logo"/>
+	<Navbar />
+	<Routes>
+		<Route path='/' exact component={Home} />
+		<Route path='/about' component={About} />
+		<Route path='/events' component={Schedule} />
+    <Route path='/schedule' component = {Checking} />
+	</Routes>
+	</Router>
+);
 }
 
 export default App;
