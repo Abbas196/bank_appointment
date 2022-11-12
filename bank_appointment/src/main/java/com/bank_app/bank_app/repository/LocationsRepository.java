699f0c92cf6a20bc8d9fbfad549e2f0cd84d4407
Aayush <axa25870@ucmo.edu>
2022-11-12 01:07:06 -0600

    The backend is ready from my part, just need to add the sort function

--- bank_appointment/src/main/java/com/bank_app/bank_app/repository/LocationsRepository.java
+++ bank_appointment/src/main/java/com/bank_app/bank_app/repository/LocationsRepository.java
@@ -1,9 +1,12 @@
 package com.bank_app.bank_app.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
 import com.bank_app.bank_app.entity.Locations;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface LocationsRepository extends JpaRepository<Locations, Long> {
+    List<Locations> findAll();
 }
