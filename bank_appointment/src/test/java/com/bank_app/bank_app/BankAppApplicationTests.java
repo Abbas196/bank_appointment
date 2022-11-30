dc6003f5912014ee5d58c5187379c5d40316ef0f
Abbas196 <55959324+Abbas196@users.noreply.github.com>
2022-11-29 23:02:40 -0600

    Revert "Hi Abbas"

--- bank_appointment/src/test/java/com/bank_app/bank_app/BankAppApplicationTests.java
+++ bank_appointment/src/test/java/com/bank_app/bank_app/BankAppApplicationTests.java
@@ -1,55 +1,35 @@
-package com.bank_app.bank_app.service;
+package com.bank_app.bank_app;
 
 import com.bank_app.bank_app.entity.Appointment;
 import com.bank_app.bank_app.entity.Customer;
 import com.bank_app.bank_app.repository.CustomerRepository;
-import org.junit.jupiter.api.*;
-import org.mockito.Mockito;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.util.Assert;
 
-import java.util.List;
-import java.util.Optional;
+import java.util.ArrayList;
 
-public class CustomerServiceTest {
+@SpringBootTest
+class BankAppApplicationTests {
 
-
-	private static CustomerService customerService;
-
-
-	@BeforeAll
-	public static void create() {
-		CustomerRepository mockCustomerRepository;
-		Customer customer;
-		Appointment appointment, appointment1, appointment2;
-		mockCustomerRepository = Mockito.mock(CustomerRepository.class);
-
-
-		appointment = new Appointment(1l, "Lee's Summit", "04:52", "11-01-2022", "Appointment with Manager", null);
-		appointment1 = new Appointment(1l, "Richardson", "03:56", "31-12-2022", "Want to apply for new passbook", null);
-		appointment2 = new Appointment(1l, "Irving", "09:00", "04-07-2022", "Want to meet clerk", null);
-
-		customer = new Customer(21212l, "Jessica", "jessica@hotmail.com", 56781234567l, List.of(appointment1, appointment2, appointment));
-
-
-		Mockito.when(mockCustomerRepository.findById(1l)).thenReturn(Optional.of(customer));
-
-		customerService=new CustomerService(mockCustomerRepository);
-
-	}
-
+	@Autowired
+	CustomerRepository customerRepository;
 
 	@Test
-	public void testGetAppointmentList(){
-		List<Appointment> appointments;
-
-		appointments=customerService.getAppointmentList(1l);
-
-		Assertions.assertEquals(3,appointments.size());
-
+	public void createCustomer(){
+		Customer customer = new Customer();
+		customer.setId(100L);
+		customer.setEmail("springBootTest@hotmail.com");
+		customer.setName("Abbas raza");
+		customer.setPhoneNumber("812793408");
+		customer.setAppointments(new ArrayList<Appointment>());
+		customerRepository.save(customer);
+		Assert.notNull(customerRepository.findById(12123L).get(), "Customer should be created");
 	}
 
-	@AfterEach
-	public void destroy(){
-		customerService=null;
-	}
+//	@Test
+//	void contextLoads() {
+//	}
 
 }
